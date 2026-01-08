4ab1b0c085d2efd29ca187ce8beaccb7bd3bce4b
Yan Carlos <yan.c.oliveira08@gmail.com>
2026-01-08 16:25:20 -0300

    Atualização de rota unica
    Atualização da rota GET para envio de informações unificado

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const data = require('../controller/sendData');
 
-router.get("/", data.enviarAbout, data.enviarProjects);
+router.get("/", data.enviarInfo);
 router.put("/admin/about/:id", data.modificarAbout);
 router.put("/admin/projects/:id", data.modificarProjects);
 router.post("/admin/projects", data.criarProjects);
+router.delete("/admin/projects/:id", data.deleteProjects);
 
 module.exports = router;
