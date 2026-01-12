6b47f897ac2eea8c41c8cbe9ed87355fa4651a3e
Yan Carlos <yan.c.oliveira08@gmail.com>
2026-01-12 20:10:07 -0300

    Adição de Formularios ao Admin
    Adição de formularios de criação e modificação de projetos, alem de um formulario de modificar o Sobre mim(about).
    As informações e funções que cada componente formulario usará esta neste mesmo arquivo admin.jsx, sendo responsavel pela logica de todos os componentes admin

--- frontend/src/components/admin/admin.jsx
+++ frontend/src/components/admin/admin.jsx
@@ -1,44 +1,107 @@
-import axios from 'axios'
 import api from '../../services/api'
 import { useState, useEffect } from 'react';
 import ProjectCard from '../projects/projectCard'
+import CreateProjectForm from './createProjectForm'
+import ModifyProjectForm from './modifyProjectForm'
+import ModifyAboutForm from './modifyAboutForm';
+
 function Admin(){
     const [dados, setDados] = useState(null);
+    const [infoProject, setInfoProject] = useState({
+        name: "",
+        description: "",
+        stack: "",
+        url: "",
+        img: "",
+    });
+    const [selectedId, setId] = useState(null);
+    const [infoAbout, setInfoAbout] = useState({
+        description: "",
+        stack: "",
+    });
 
     const carregarDados = () =>  {
         api.get("/")
         .then(res => {
-            console.log(res.data);
             setDados(res.data);
         })
         .catch(err => console.error(err));
     }
-
     useEffect(() => {
         carregarDados();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        if(!selectedId || !dados) return;
+
+        const project = dados.projects.find(p => p._id === selectedId);
+        if(project){
+            setInfoProject({
+                name: project.name,
+                description: project.description,
+                stack: project.stack,
+                url: project.url,
+                img: project.img,
+            });
+        }
+
+    }, [selectedId, dados]);
+
+    useEffect(() => {
+        const about = dados?.about?.[0];
+        if(about){
+            setInfoAbout({
+                description: about.description,
+                stack: about.stack,
+            });
+        }
+
+    }, [dados])
 
     const criarProject = async (e) => {
         e.preventDefault();
 
-        const corpo = {
-            name: "teste",
-            description: "projeto teste",
-            stack: ["Node.js", "marmelada"],
-            url: "asdhfasfd.com",
-            img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%2Fid%2FOIP.rLtRXRXU7-pAc9RbY7xTqAHaEK%3Fpid%3DApi&f=1&ipt=30cf29826283a818d076907f3f7d3071c7535809af18e3c2799fe1c76eca8d95&ipo=images"
-        };
-
-        const response = await axios.post('http://localhost:3000/admin/projects', corpo)
+        const response = await api.post('/admin/projects', infoProject);
         carregarDados();
-        console.log(response.data)
+    }
+    const modificarProjeto = async(e) => {
+        e.preventDefault();
+
+        const response = await api.put(`/admin/projects/${selectedId}`, infoProject);
+        carregarDados();
+    }
+    const modificarAbout = async(e) => {
+        e.preventDefault();
+        const idAbout = dados?.about?.[0]?._id;
+
+        const response = await api.put(`/admin/about/${idAbout}`, infoAbout);
+        if(response) alert("bem sucedido!");
+        carregarDados();
     }
 
     return(
-        <div>
-           <form onSubmit={criarProject}>
-                <button type="submit">Salvar projeto</button>
-           </form>
+        <div className='flex flex-col gap-20'>
+            <div className='flex gap-10'>
+                <CreateProjectForm 
+                    criarProject={criarProject}
+                    setInfoProject={setInfoProject}
+                />
+                
+                <ModifyAboutForm
+                    dados={dados}
+                    setInfoAbout={setInfoAbout}
+                    modificarAbout={modificarAbout}
+                    infoAbout={infoAbout}
+                />
+
+                <ModifyProjectForm 
+                    dados={dados} 
+                    setInfoProject={setInfoProject} 
+                    modificarProjeto={modificarProjeto} 
+                    id={selectedId}
+                    infoProject={infoProject}
+                />
+            </div>
             <div className='flex-wrap flex relative gap-4'>
                 {dados && dados.projects && dados.projects.map(project => (
                 <ProjectCard
@@ -48,6 +111,7 @@ function Admin(){
                     url={project.url}
                     id={project._id}
                     carregarDados={carregarDados}
+                    setId={setId}
                 />
             ))}
             </div>
