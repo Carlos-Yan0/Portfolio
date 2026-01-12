17bb8d1ce78bd06acf9f07866f835b95295e3193
Yan Carlos <yan.c.oliveira08@gmail.com>
2026-01-12 20:07:54 -0300

    adição de um componente About
    adição de um componente About que vai conter minha stack principal tambem uma descrição sobre mim

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -1,7 +1,28 @@
-import { useEffect } from "react";
-import axios from 'axios';
+import api from '../../services/api'
+import { useEffect, useState } from 'react';
 function About(){
-    useEffect(
-        axios.get()
+    const [dados, setDados] = useState(null)
+
+    useEffect(() => {
+        api.get("/")
+        .then(res => {
+            setDados(res.data);
+            console.log(JSON.stringify(res.data, null, 2))
+        })
+        .catch(err => console.log(err))
+    }, []);
+
+    return(
+        <div className='flex gap-10 w-screen h-screen'>
+            <div>
+                <h1 className='text-5xl'>Sobre mim</h1>
+                <p className='text-2xl'>{dados?.about?.[0]?.description}</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
     );
 }
+
+export default About;
